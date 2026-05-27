e38f79b6af59332516b1b459519e28d03ecebef3
Sinchana KJ <sinchanakj26@gmail.com>
2026-05-27 21:18:25 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        unordered_map<string,vector<string>>m;
+        for(int i=0;i<strs.size();i++)
+        {
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            m[temp].push_back(strs[i]);
+        }
+
+        vector<vector<string>>ans;
+        for(auto it :m)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
